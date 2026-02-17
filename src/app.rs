419584f6efd6b0160674b83a0b9618ecd663ed66
RoboCherry <150583047+RoboCherry@users.noreply.github.com>
2026-02-17 22:14:59 +0000

    various shit
    -pressing the button will now add dummy text
    -can now quit with Esc

--- src/app.rs
+++ src/app.rs
@@ -1,20 +1,16 @@
+use egui::{Context, FontFamily, FontId, Key, RichText, TextStyle};
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct ChauncerApp {
-    // Example stuff:
-    label: String,
-
-    #[serde(skip)] // This how you opt-out of serialization of a field
-    value: f32,
+    apps : Vec<String>
 }
 
 impl Default for ChauncerApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            apps : Vec::new()
         }
     }
 }
@@ -43,64 +39,53 @@ impl eframe::App for ChauncerApp {
 
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
+        //set_styles(ctx);
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
 
             egui::MenuBar::new().ui(ui, |ui| {
                 ui.menu_button("File", |ui| {
-                    if ui.button("Quit").clicked() {
+                    if ui.button("Quit").clicked(){
                         ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+                    }
+                    if ui.button("Fullscreen").clicked() {
+                        ctx.send_viewport_cmd(egui::ViewportCommand::Fullscreen(true));
+                    }
+                    if ui.button("Hover Window").clicked() {
+                        ctx.send_viewport_cmd(egui::ViewportCommand::Fullscreen(false));
                     }
                 });
                     ui.add_space(16.0);
 
 
-                egui::widgets::global_theme_preference_buttons(ui);
+                //egui::widgets::global_theme_preference_buttons(ui);
             });
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
-
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
+            ui.heading("Applications");
+            
+            if ui.input(|i| i.key_pressed(Key::Escape)){
+                ctx.send_viewport_cmd(egui::ViewportCommand::Close);
             }
-
-            ui.separator();
-
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
-            });
+            if ui.button("Delete Apps [-]").clicked(){
+                self.apps = Vec::new()
+            }
+            if ui.button("Add App [+]").clicked() {
+                let _ = &mut self.apps.push("heyo".to_string());
+            }
+            
+            for i in &mut self.apps.iter(){
+                ui.label(RichText::new(i));
+            }
         });
     }
 }
 
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
-    });
+
+fn set_styles(ctx: &Context) {
+    let mut style = (*ctx.style()).clone();
+    style.text_styles = [(TextStyle::Heading,FontId::new(30.0, FontFamily::Monospace))].into();
+    ctx.set_style(style);
+    
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() -> eframe::Result {
 
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
+            .with_fullscreen(false)
             .with_inner_size([400.0, 300.0])
             .with_min_inner_size([300.0, 220.0])
             .with_icon(
